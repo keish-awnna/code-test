e1c1846e0f9baddaae4cfb35eee6920a19a2a505
keishawnna <keishawnna@gradientascent.io>
2023-07-16 20:23:32 -0700

    adding assets and styling header

--- src/components/ui/molecules/Jumbotron.jsx
+++ src/components/ui/molecules/Jumbotron.jsx
@@ -4,10 +4,16 @@ const Jumbotron = () => {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="jumbo-content-wrapper">
-        <div className="container">
-          <h1 className="display-4">JUMBOTRON</h1>
-          <p className="lead">apartment complex</p>
+        <div className="tag-line-content">
+          <h3 className="tag-line">
+            Experience the Best of San Francisco Living at
+          </h3>
+          <h1 className="display-4">Chancellor Apartments</h1>
+          {/* <h3 className="lead">in San Francisco</h3> */}
         </div>
+        {/* <div className="call-to-action">
+          <h3 className="cta-title">View Floor Plans</h3>
+        </div> */}
       </div>
     </div>
   );
--- src/components/ui/molecules/NavBar.jsx
+++ src/components/ui/molecules/NavBar.jsx
@@ -67,7 +67,7 @@ const NavBar = () => {
           <div className="navbar-content-wrapper">
             <div className="row">
               <div className="col-lg-6 col-md-6 col-sm-6">
-                <div className="left-side-content">Chancellor Apartments</div>
+                <div className="left-side-content">Chancellor</div>
               </div>
               <div className="col-lg-6 col-md-6 col-sm-6">
                 <div className="right-side-content">
