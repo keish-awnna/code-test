eeb59b17ebaa378970704807c1088cc4f5a326db
keishawnna <keishawnna@gradientascent.io>
2023-07-18 07:10:30 -0700

    wrapped bottom hero in a container

--- src/components/ui/molecules/homePageComps/BottomHero.jsx
+++ src/components/ui/molecules/homePageComps/BottomHero.jsx
@@ -4,12 +4,14 @@ const BottomHero = () => {
   return (
     <section className="bottom-hero-wrapper">
       <div className="hero-overlay">
-        <div className="btm-hero-content">
-          <h3 className="btm-hero-title">
-            Welcome home to Chancellor Apartments, where luxury and comfort
-            await you.
-          </h3>
-          <button className="cta-btn">Apply Now</button>
+        <div className="container">
+          <div className="btm-hero-content">
+            <h3 className="btm-hero-title">
+              Welcome home to Chancellor Apartments, where luxury and comfort
+              await you.
+            </h3>
+            <button className="cta-btn">Apply Now</button>
+          </div>
         </div>
       </div>
     </section>
